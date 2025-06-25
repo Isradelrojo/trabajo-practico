c41d72d6ec6303711ed8b7c89edcaabd6f376403
Isradelrojo <isradelrojo@gmail.com>
2025-06-25 19:35:51 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const boton = document.getElementById("hamburguesa");
+const menuDesplegado = document.getElementById("container");
+const brand = document.getElementById("brand");
+const asideOpen = document.getElementById("aside");
+const menuOpen = document.getElementById("menu");
+
+
+
+
+boton.addEventListener("click", ()=>{
+    menuDesplegado.classList.toggle("menu__desplegado");
+    asideOpen.classList.toggle("aside__open");
+    brand.classList.toggle("brand_open");
+    menuOpen.classList.toggle("menu_open");
+});
